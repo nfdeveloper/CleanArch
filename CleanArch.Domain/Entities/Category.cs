4231b1fc26a45caf06c9cc5111752988d666bda6
Nilton F. <nfdeveloper04@gmail.com>
2024-04-28 17:31:51 -0300

    Enriquecendo o modelo de domínio Category

--- CleanArch.Domain/Entities/Category.cs
+++ CleanArch.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArch.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,34 @@ using System.Threading.Tasks;
 
 namespace CleanArch.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Id inválido.");
+            Id = id;    
+            ValidateDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+    
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Nome da Categoria é obrigatório.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Nome da Categoria tem que ser maior que 3 caracteres");
+
+            Name = name;
+        }
     }
 }
