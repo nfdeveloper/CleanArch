b0e33df38eec18f3dc27e4b83bb86ed8ff5d0ede
Nilton F. <nfdeveloper04@gmail.com>
2024-04-29 00:33:23 -0300

    Criando as Interfaces dos Repositorios de Product e Category.

---
+++ CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategories();
+        Task<Product> GetById(int? id);
+
+        Task<Product> GetProductCategory(int? id);
+
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task Remove(Product product);
+    }
+}
